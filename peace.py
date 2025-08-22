3b8bcdebd608820a76392673b1c8ce09e9c9050e
r246499h-a11y <r246499h@cuz.ac.zw>
2025-08-22 14:47:38 +0200

    Add files via upload

---
+++ peace.py
@@ -0,0 +1,11 @@
+#constant
+speed = 29 # in meters per second
+
+# Define a variable
+time_seconds = 60  # time in seconds
+
+# Calculate distance
+distance = speed * time_seconds
+
+# Print the result
+print("Distance traveled in 10 seconds:", distance, "meters")
